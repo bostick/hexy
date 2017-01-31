f8364d06b02067ba7b559be15fd7df432a6a113a
Brenton Bostick <bostick@gmail.com>
2017-01-31 09:03:28 -0500

    Add project to git

---
+++ Assets/Scripts/CollisionLogger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionLogger : MonoBehaviour {
+
+		void OnTriggerEnter(Collider other) {
+			//Destroy (other.gameObject);
+	
+	//		if (other.gameObject.CompareTag("Pick Up")) {
+	//			other.gameObject.SetActive (false);
+	//			count = count + 1;
+	//			SetCountText ();
+	//		}
+	
+			//Debug.Log ("Trigger!");
+	
+		}
+
+	void OnCollisionEnter(Collision collision) {
+		
+		//ContactPoint p = collision.contacts [0];
+
+		//Debug.Log ("Collision! " + (p.thisCollider.gameObject.tag) + " " + (p.otherCollider.gameObject.tag) + " " + (p.thisCollider.gameObject.transform.position.ToString()) + " " + (p.thisCollider.gameObject.transform.rotation.ToString()));
+	}
+
+}
---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Rigidbody rb = GetComponent<Rigidbody> ();
+
+		//Vector3 NewPosition2 = rb.transform.TransformPoint(new Vector3(0f, 0f, -1f));
+		//rb.AddForceAtPosition(rb.transform.forward * 100f, NewPosition2, ForceMode.Force);
+
+		rb.AddForce (rb.transform.forward * 600f);
+	}
+}
